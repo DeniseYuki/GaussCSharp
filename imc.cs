74ea18e241f646f1a9288b565c2e9c3e5ac8645d
DeniseYuki <49623035+DeniseYuki@users.noreply.github.com>
2023-04-03 15:53:03 -0300

    Create imc.cs

---
+++ imc.cs
@@ -0,0 +1,40 @@
+using System.Data;
+using System.Diagnostics.SymbolStore;
+
+Console.WriteLine(" Digite o seu peso: ");
+
+float peso = float.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite sua altura: ");
+
+float altura = float.Parse(Console.ReadLine());
+float alturaAoQuadrado = altura * altura;
+float imc = peso / alturaAoQuadrado;
+
+Console.WriteLine("O IMC é: " + imc);
+
+if (imc < 18.5)
+{
+    Console.WriteLine("O IMC está abaixo do peso");
+}
+else
+{
+    if (imc > 18.6 && imc < 24.9)
+    {
+        Console.WriteLine(" Peso ideal");
+    }
+    else
+    {
+        if (imc > 25 && imc < 29.9)
+        {
+            Console.WriteLine("Acima do peso");
+        }
+        else
+        {
+            Console.WriteLine("Obesidade");
+        }
+
+        Console.WriteLine("O IMC é: " + imc);
+
+    }
+}
