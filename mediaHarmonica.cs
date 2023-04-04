49d1b5b7b6ce5f4ae958f0d76c75bb5a276525c4
DeniseYuki <49623035+DeniseYuki@users.noreply.github.com>
2023-04-03 22:18:19 -0300

    Create mediaHarmonica.cs

---
+++ mediaHarmonica.cs
@@ -0,0 +1,10 @@
+float media, fracao1, fracao2, fracao3, somaFracao;   
+float n = 3f,num1 = 4f,num2 = 5f, num3 = 8f;
+
+  fracao1 = (1/num1);
+  fracao2 = (1/num2);
+  fracao3 = (1/num3);
+
+  somaFracao = fracao1 +fracao2 + fracao3;
+  media = n / somaFracao;
+  Console.WriteLine("a média harmonica é : " + media);
