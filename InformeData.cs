a25ef3c8e08c364ece9ba8b20ee1053ced2ad2f7
DeniseYuki <49623035+DeniseYuki@users.noreply.github.com>
2023-04-05 21:18:55 -0300

    Add files via upload

---
+++ InformeData.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+
+namespace informeAno;
+class Program
+{
+    static void Main(string[] args)
+    {
+        DateTime data;
+        Console.WriteLine("DIGITE UMA DATA: ");
+        data = DateTime.Parse(Console.ReadLine());
+        Console.WriteLine(data);
+        Console.WriteLine(data.ToString("MM"));
+
+    }
+}
