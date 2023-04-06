355be9737601d16d6e47dda5c7dc38f12e6e4ae2
DeniseYuki <49623035+DeniseYuki@users.noreply.github.com>
2023-04-05 21:12:58 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace informeAno;
+class Program
+{
+    static void Main(string[] args)
+    {
+        DateTime data;
+        Console.WriteLine("DIGITE UMA DATA: ");
+        data = DateTime.Parse(Console.ReadLine());
+        Console.WriteLine(data);
+        Console.WriteLine(data.ToString("yyyy"));
+        
+    }
+}
+
