f93d5712a4f2417d8b30f9700825ede3eb646cce
DeniseYuki <49623035+DeniseYuki@users.noreply.github.com>
2023-04-05 21:43:45 -0300

    Add files via upload

---
+++ distanciaDoDez.cs
@@ -0,0 +1,23 @@
+﻿int num = 10, num1, distancia;
+
+Console.WriteLine(" Digite um valor: ");
+num1 = int.Parse(Console.ReadLine());
+
+distancia = num - num1;
+
+Console.WriteLine(distancia);
+
+if (num1 > 10){ 
+
+    distancia = num1 - num;
+    Console.WriteLine(" a distancia em módulo é : "+distancia);
+}
+else
+{
+    distancia = num - num1;
+    Console.WriteLine("A distancia é : ",distancia);
+
+}
+
+
+
