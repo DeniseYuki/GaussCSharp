657983f40e5d5c17afa4fb9695f98b5154e2f6cf
DeniseYuki <49623035+DeniseYuki@users.noreply.github.com>
2023-04-05 21:25:37 -0300

    Add files via upload

---
+++ InformeDataAtual.cs
@@ -0,0 +1,13 @@
+﻿using System;
+ namespace informeDiaAtual;
+
+class program
+{
+    static void Main (string[] args)
+    {
+        DateTime data = DateTime.Now;
+
+        Console.WriteLine(data.ToString("dd/MM/yyyy"));
+    }
+}
+
