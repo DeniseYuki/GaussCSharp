e469973403e8cdfc8d8680bb9c3378668d760311
DeniseYuki <49623035+DeniseYuki@users.noreply.github.com>
2023-04-05 22:08:12 -0300

    Update imc.cs

--- imc.cs
+++ imc.cs
@@ -1,3 +1,5 @@
+
+ 
 using System.Data;
 using System.Diagnostics.SymbolStore;
 
@@ -5,32 +7,28 @@ Console.WriteLine(" Digite o seu peso: ");
 
 float peso = float.Parse(Console.ReadLine());
 
-Console.WriteLine("Digite sua altura: ");
+Console.WriteLine("Digite sua altura:  com (.) ex : 1.62");
 
 float altura = float.Parse(Console.ReadLine());
 float alturaAoQuadrado = altura * altura;
 float imc = peso / alturaAoQuadrado;
 
-Console.WriteLine("O IMC é: " + imc);
-
 if (imc < 18.5)
 {
-    Console.WriteLine("O IMC está abaixo do peso");
+    Console.WriteLine("O IMC está abaixo do peso e o imc é : " + imc);
 }
-if (imc > 18.6 && imc < 24.9)
+else if (imc > 18.6 && imc < 24.9)
 {
-    Console.WriteLine(" Peso ideal");
+
+    Console.WriteLine(" Peso ideal e o imc é : " + imc);
 }
-if (imc > 25 && imc < 29.9)
-
+else if (imc > 25 && imc < 29.9)
 {
-    Console.WriteLine("Acima do peso");
 
+    Console.WriteLine("Acima do peso e o imc é : " + imc);
 }
 else
 
 {
-    Console.WriteLine("Obesidade");
- }
-    
- 
+    Console.WriteLine("Obesidade e o imc é : " + imc);
+}
