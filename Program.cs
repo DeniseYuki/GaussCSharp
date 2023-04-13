557e0c0b6f0f0c12f690c16b35b8ffd20ad292c5
DeniseYuki <49623035+DeniseYuki@users.noreply.github.com>
2023-04-13 10:06:43 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+class Program
+{
+
+    public class Livro
+    {
+
+        public Livro() { }
+
+        public string Prato { get; set; }
+        public string TipoPrato { get; set; }
+
+        public Livro(string Prato, string TipoPrato)
+        {
+            this.Prato = Prato;
+            this.TipoPrato = TipoPrato;
+        }
+
+        static List<Livro> receitas;
+
+        static void Main(string[] args)
+        {
+            receitas = new List<Livro>();
+
+            receitas.Add(new Livro("brigadeiro", "doce"));
+            receitas.Add(new Livro("morango", "doce"));
+            receitas.Add(new Livro("arroz", "salgado"));
+            receitas.Add(new Livro("feijoada", "salgado"));
+            //receitas.Add(new Livro(null, null));
+
+            foreach (Livro l in receitas)
+            {
+
+                Console.WriteLine(l.Prato +" "+ l.TipoPrato);
+            }
+
+
+
+
+        }
+    }
+}
+
+
+
+
+   // public class Livro
+    
+
+      //  public Livro() { }
+
+       // public string Prato { get; set; }
+       // public string TipoPrato { get; set; }
+
+      //  public Livro(string Prato, string TipoPrato)
+        
+        //    this.Prato = Prato;
+        //    this.TipoPrato = TipoPrato;
+       // }
+
+
+    
+
