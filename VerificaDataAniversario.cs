2d26ba3b29c30d016a0f3cde3544ba4a5100bbed
DeniseYuki <49623035+DeniseYuki@users.noreply.github.com>
2023-04-12 22:30:24 -0300

    Add files via upload

---
+++ VerificaDataAniversario.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+class Program
+{
+    static void Main(string[] args)
+    {
+        DateTime data;
+
+        CultureInfo cultureBR = new CultureInfo("pt-BR");
+
+        DateTime aniversario = new DateTime(1990, 03, 08);
+
+        Console.WriteLine("Digite uma data:  (dia/mês/ano)" );
+
+        data = DateTime.Parse(Console.ReadLine());
+
+        string dataFormatada = data.ToString("G", cultureBR);
+
+
+        int compare = DateTime.Compare(aniversario,data);
+
+        if(aniversario == data)
+        {
+            Console.WriteLine(" É a data do seu aniversário");
+        }
+        else
+        {
+
+            Console.WriteLine(" Não é a data do seu aniversário");
+        }
+
+    }
+}
