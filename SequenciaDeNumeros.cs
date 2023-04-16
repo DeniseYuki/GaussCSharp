4b0525b5f4ddd4e8d36feae70cd6c76d53640235
DeniseYuki <49623035+DeniseYuki@users.noreply.github.com>
2023-04-16 15:38:33 -0300

    Add files via upload

---
+++ SequenciaDeNumeros.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+
+List<int> valores = new List<int>();
+
+int i = 0, a = 0;
+while (i < 10) {
+    // valores.Add(Int32.Parse(Console.ReadLine()));
+    valores.Add(i);
+    i += 1;
+    foreach (int itens in valores) ;
+
+    Console.WriteLine(i);
+
+        }
+
