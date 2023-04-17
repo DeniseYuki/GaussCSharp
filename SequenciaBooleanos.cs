a2ac15cb429b0a46413f32137acacab989116f62
DeniseYuki <49623035+DeniseYuki@users.noreply.github.com>
2023-04-16 22:24:23 -0300

    Add files via upload

---
+++ SequenciaBooleanos.cs
@@ -0,0 +1,31 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+
+
+internal class Program
+{
+    private static bool valores;
+
+    private static void Main(string[] args)
+    {
+        var valor = new List<bool>();
+
+        int i = 0;
+        while (i < 5) {
+            //valor.Add(false);
+            valor.Add(true);
+            foreach (var valores in valor)
+                i += 1;
+            { 
+        Console.WriteLine(valores);
+               
+
+             }
+
+        }
+
+
+    }
+}
