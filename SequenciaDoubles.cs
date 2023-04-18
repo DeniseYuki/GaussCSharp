0ad2a660126be7a36c0a4fd36dd33b8c21eed7d8
DeniseYuki <49623035+DeniseYuki@users.noreply.github.com>
2023-04-18 20:44:10 -0300

    Add files via upload

---
+++ SequenciaDoubles.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+internal class Program
+{
+
+    private static double sequencia;
+
+    private static void Main(string[] args)
+    {
+        var valor = new List<double>() { 1.3, 1.5, 3.5, 3.6, 5.5 };
+
+        foreach (var sequencia in valor)
+        {
+            Console.WriteLine(sequencia);
+
+        }
+    }
+}
