83d15161c1493c9e0d4439ad7db55c438aff97f6
DeniseYuki <49623035+DeniseYuki@users.noreply.github.com>
2023-04-19 12:21:07 -0300

    Add files via upload

--- AgendaTelefonica.cs
+++ AgendaTelefonica.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.Intrinsics.X86;
 
 class program
 {
@@ -23,8 +24,10 @@ class program
 
         static List<AgendaTelefonica>? contato;
         private static int telefone;
+        private static object or;
 
         static void Main(string[] args)
+
 
         {
             {
@@ -32,6 +35,7 @@ class program
                 contato = new List<AgendaTelefonica>();
 
                 {
+
                     Console.WriteLine("Digite um nome: ");
                     nome = string.Format(Console.ReadLine());
 
@@ -43,6 +47,36 @@ class program
 
                     foreach (AgendaTelefonica a in contato)
                         Console.WriteLine("Nome: " + a.Nome + " cel: 11 " + a.Telefone);
+
+                    Console.WriteLine("Adicionar mais um número? [aperte S para continuar ou qualquer outra tecla para sair]");
+                    string c = Console.ReadLine();
+
+                    while (c == "S")
+                    {
+                        Console.WriteLine("Adicionar mais um número? [aperte S para continuar ou qualquer outra tecla para sair]");
+                        string d = Console.ReadLine();
+                        
+                        if (c == "S")
+                        {
+                            Console.WriteLine("Digite um nome: ");
+                            nome = string.Format(Console.ReadLine());
+
+                            Console.WriteLine("Digite um número: ");
+                            telefone = Int32.Parse(Console.ReadLine());
+
+
+                            contato.Add(new AgendaTelefonica(nome, telefone));
+
+                            foreach (AgendaTelefonica a in contato)
+                                Console.WriteLine("Nome: " + a.Nome + " cel: 11 " + a.Telefone);
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+
+
                 }
             }
 
