91f2bb0c7fc7a96b440c66326ddda192b5d81c7a
DeniseYuki <49623035+DeniseYuki@users.noreply.github.com>
2023-05-01 19:55:13 -0300

    Add files via upload

---
+++ Carro.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace carro
+{
+	public class Carro
+	{
+		public string modelo;
+		public int ano;
+		public float km;
+		
+	}
+
+	public class Motor
+	{
+		public string modelo;
+		public string tipo;
+		public int tamanho;
+	}
+}
+
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace carro;
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        var carro = new Carro();
+
+        carro.modelo = "Gol";
+        carro.ano = 2000;
+        carro.km = 50.3f;
+
+        var carro1 = new Carro();
+
+        carro1.modelo = "Uno";
+        carro1.ano = 2015;
+        carro1.km = 25.6f;
+
+        var motor = new Motor();
+
+        motor.modelo = "V8";
+        motor.tipo = "xyz";
+        motor.tamanho = 10;
+
+        Console.WriteLine("O Carro  " + carro.modelo + " ano:  " + carro.ano + "  com a quantidade em km:  " + carro.km);
+        Console.WriteLine("O Carro  " + carro1.modelo + " ano:  " + carro1.ano + " com a quantidade em km:  " + carro1.km);
+        Console.WriteLine("O motor:  " + motor.tipo + " é do tipo  " + motor.tipo + " e do tamanho  " + motor.tamanho);
+    }
+}
+
