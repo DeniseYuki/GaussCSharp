fb30fe93aa84a5b9d958f9703df7df176a2bd4a4
DeniseYuki <49623035+DeniseYuki@users.noreply.github.com>
2023-05-02 11:10:45 -0300

    Update numeroDecrescente.cs

--- numeroDecrescente.cs
+++ numeroDecrescente.cs
@@ -1,10 +1,21 @@
+// Primeira maneira usando WHILE: (não estava. no exercicio, mas foi o primeiro que eu imaginei, depois eu li direito e fiz o segundo)
+
 int num = 101, i = 0;
-List<int> numeros = new List<int>();
 while (i < 100)
 {
     num --;
     numeros.Add(num);
     Console.WriteLine(num);
     i++;
+}
+
+
+
+
+// 2 maneira usando o FOR:
+
+for (int a = 100; a > 0; a--)
+{
+    Console.WriteLine(a);
 }
 
