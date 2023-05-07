62c54da7936f15b83a1c5d5b5334bd350361be0b
DeniseYuki <49623035+DeniseYuki@users.noreply.github.com>
2023-05-07 15:59:26 -0300

    Create cadastro.cs

---
+++ cadastro.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pessoa.modelo
+{
+    public class Cadastro
+    {
+        public string nome;
+        public string funcao;
+        public string educação;
+        public float salario;
+        public float tempoDeServico;
+
+    }
+    public class TotalFuncionarios
+    {
+        List<string> lista = new List<string>();
+
+        public void Add(string nome)
+        {
+          
+        }
+    }
