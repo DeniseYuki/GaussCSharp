6614cae77f160e11554d7d69c321f746d7e7f607
DeniseYuki <49623035+DeniseYuki@users.noreply.github.com>
2023-05-08 14:24:20 -0300

    Update cadastro1.cs

--- cadastro1.cs
+++ cadastro1.cs
@@ -6,62 +6,115 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-Cadastro Cadastro1 = new Cadastro();
-Cadastro1.id = 1;
-Cadastro1.nome = "jose";
-Cadastro1.funcao = "Enfermeiro";
-Cadastro1.educacao = "Ensino médio";
-Cadastro1.salario = 1290.90f;
-Cadastro1.tempoDeServico = 8f;
+//// Empresa MaxAlteirTesla
+//// cargos: Gerente [2], Atendente [4] , rh[3], suporte[2].
+////média salarial por setor
+////Registro de tempo por setor
+//// cadastro e listagem por setor
 
-Cadastro Cadastro2 = new Cadastro();
-Cadastro2.id = 2;
-Cadastro2.nome = "Maria";
-Cadastro2.funcao = "médica";
-Cadastro2.educacao = "Ensino superior";
-Cadastro2.salario = 32900.90f;
-Cadastro2.tempoDeServico = 4f;
 
-Cadastro Cadastro3 = new Cadastro();
-Console.WriteLine("ID");
-Cadastro3.id =
-Int32.Parse(Console.ReadLine());
-
-Console.WriteLine("NOME: ");
-Cadastro3.nome =
-Console.ReadLine();
-
-Console.WriteLine("FUNÇÃO: ");
-Cadastro3.funcao =
-Console.ReadLine();
-
-Console.WriteLine("ESCOLARIDADE: ");
-Cadastro3.educacao =
-Console.ReadLine();
-
-Console.WriteLine("SALARIO: ");
-Cadastro3.salario =
-float.Parse(Console.ReadLine());
-
-Console.WriteLine("HORAS TRABALHADAS NO DIA:  ");
-Cadastro3.tempoDeServico =
-float.Parse(Console.ReadLine());
-
-List<string> lista = new List<string>();
-lista.Add(Cadastro1.id + " : " + Cadastro1.nome);
-lista.Add(Cadastro2.id + " : " + Cadastro2.nome);
-lista.Add(Cadastro3.id + " : " + Cadastro3.nome);
-
-foreach (var Pessoa in lista)
+namespace EmpresaMaxAlteirTesla
 {
-    Console.WriteLine("\n"+Pessoa);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Criação dos cadastros
+            Cadastro Cadastro1 = new Cadastro();
+            Cadastro1.id = 1;
+            Cadastro1.nome = "Jose";
+            Cadastro1.funcao = "Gerente";
+            Cadastro1.educacao = "Ensino superior";
+            Cadastro1.salario = 1290.90f;
+            Cadastro1.tempoDeServico = 8f;
+
+            Cadastro Cadastro2 = new Cadastro();
+            Cadastro2.id = 2;
+            Cadastro2.nome = "Maria";
+            Cadastro2.funcao = "Suporte";
+            Cadastro2.educacao = "Ensino superior";
+            Cadastro2.salario = 32900.90f;
+            Cadastro2.tempoDeServico = 4f;
+
+            // Criação de uma lista para armazenar os cadastros
+            List<Cadastro> Cadastros = new List<Cadastro>();
+            Cadastros.Add(Cadastro1);
+            Cadastros.Add(Cadastro2);
+
+            int i = 3;
+            while (i < 11)
+            {
+                Cadastro CadastroAdicional = new Cadastro();
+                Console.WriteLine("ID: ");
+                CadastroAdicional.id = (i);
+
+                Console.WriteLine("NOME: ");
+                CadastroAdicional.nome = Console.ReadLine();
+
+                Console.WriteLine("FUNÇÃO: ");
+                CadastroAdicional.funcao = Console.ReadLine();
+
+                Console.WriteLine("ESCOLARIDADE: ");
+                CadastroAdicional.educacao = Console.ReadLine();
+
+                Console.WriteLine("SALARIO: ");
+                CadastroAdicional.salario = float.Parse(Console.ReadLine());
+
+                Console.WriteLine("HORAS TRABALHADAS NO DIA: ");
+                CadastroAdicional.tempoDeServico = float.Parse(Console.ReadLine());
+
+                Cadastros.Add(CadastroAdicional);
+                i++;
+                Console.Clear();
+            }
+
+            Console.WriteLine("\n ====== CADASTROS ====== \n");
+            foreach (var Cadastro in Cadastros)
+            {
+                Console.WriteLine($"ID: {Cadastro.id}");
+                Console.WriteLine($"NOME: {Cadastro.nome}");
+                Console.WriteLine($"FUNÇÃO: {Cadastro.funcao}");
+                Console.WriteLine($"ESCOLARIDADE: {Cadastro.educacao}");
+                Console.WriteLine($"SALÁRIO: {Cadastro.salario}");
+                Console.WriteLine($"HORAS TRABALHADAS NO DIA:");
+
+                // media salario por setor:
+
+                if (Cadastro.funcao == "Gerente")
+                {
+                    float mediaSalario = (Cadastro.salario++)/2;
+                    Console.WriteLine("\n "+mediaSalario);
+
+                }
+
+                else if(Cadastro.funcao == "Atendente")
+                {
+                    float mediaSalario = (Cadastro.salario++)/4;
+                    Console.WriteLine("\n "+mediaSalario);
 
 
+                }
+
+                else if (Cadastro.funcao =="RH") {
+                    float mediaSalario = (Cadastro.salario++)/3;
+                    Console.WriteLine("/n "+mediaSalario);
+
+                }
+
+                else
+                {
+                    float mediaSalario = (Cadastro.salario++)/2;
+                    Console.WriteLine("/n " + mediaSalario);
+
+
+                }
+
+
+
+
+
+            }
+        }
+    }
 }
-
-
-Console.WriteLine("\nCADASTRO 1 \n" + Cadastro1.nome + "\n" + Cadastro1.funcao + "\n" + Cadastro1.educacao + "\n" + Cadastro1.salario + "\n" + Cadastro1.tempoDeServico + " horas \n");
-Console.WriteLine("CADASTRO 2 \n" + Cadastro2.nome + "\n" + Cadastro2.funcao + "\n" + Cadastro2.educacao + "\n" + Cadastro2.salario + "\n" + Cadastro2.tempoDeServico + " horas \n");
-Console.WriteLine("\nCADASTRO 3 \n" + Cadastro3.nome + "\n" + Cadastro3.funcao + "\n" + Cadastro3.educacao + "\n" + Cadastro3.salario + "\n" + Cadastro3.tempoDeServico + " horas \n");
-
 
